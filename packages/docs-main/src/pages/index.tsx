f83b726b4621b7195c577e1902aaf92b799e9e3e
Max Rohde <maxrohde.public@gmail.com>
2020-09-19 09:08:31 +1000

    Implementing header

--- packages/docs-main/src/pages/index.tsx
+++ packages/docs-main/src/pages/index.tsx
@@ -8,44 +8,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-const fetcher = (url: string): any => fetch(url).then((r) => r.json());
-
-const FetchedContent = (): JSX.Element => {
-  const { data, error } = useSWR(
-    'https://jsonplaceholder.typicode.com/todos/1',
-    fetcher
-  );
-
-  if (error) {
-    return <p>Cannot load data</p>;
-  }
-
-  if (!data) {
-    return <p>Loading ...</p>;
-  }
-
-  return <>{data && <p>Data: {'' + data.title}</p>}</>;
-};
+import Header from 'src/components/Header';
 
 const HomePage = (): JSX.Element => {
-  const [displayData, setDisplayData] = useState(false);
-
-  const toggleData = (): void => {
-    setDisplayData(!displayData);
-  };
-
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Next.js React Bootstrap</h1>
-          <p>Welcome to Next.js with Bootstrap!</p>
-          <p>{/* <img src={BootstrapIcon} /> */}</p>
-          <Button onClick={toggleData}>Toggle Data Display</Button>
-          {displayData && <FetchedContent />}
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Header></Header>
+    </>
   );
 };
 
