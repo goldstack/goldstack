c66ac0fb7f554c3aba5858524dbec523a9610c83
Max Rohde <maxrohde.public@gmail.com>
2020-10-10 12:34:30 +1100

    add_tracking

--- packages/docs-main/src/pages/_app.tsx
+++ packages/docs-main/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './../styles/fonts.css';
 import './../styles/app.css';
@@ -7,6 +7,11 @@ import './../styles/docs.css';
 import './../styles/goldstack-docs.css';
 
 import { ThemeProvider } from 'styled-components';
+
+import { useRouter } from 'next/router';
+import { initGtm, pageview } from '@goldstack/utils-track';
+
+import { init } from '@fullstory/browser';
 
 const theme = {
   colors: {
@@ -17,6 +22,34 @@ const theme = {
 const BootstrapApp = ({ Component, pageProps }): JSX.Element => {
   process.env.GOLDSTACK_DEPLOYMENT =
     process.env.NEXT_PUBLIC_GOLDSTACK_DEPLOYMENT;
+  initGtm('UA-180192522-1');
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.GOLDSTACK_DEPLOYMENT === 'prod') {
+      init({ orgId: 'YN5JJ' });
+    }
+  }, []);
+
+  // see https://medium.com/frontend-digest/using-nextjs-with-google-analytics-and-typescript-620ba2359dea
+  useEffect(() => {
+    let handleRouteChange: any = undefined;
+    if (process.env.GOLDSTACK_DEPLOYMENT === 'prod') {
+      handleRouteChange = (url): void => {
+        pageview({
+          // path: router.pathname,
+          path: url, // Use URL as path since automatically resolved document page names should include the resolved ids
+          url: url,
+        });
+      };
+      router.events.on('routeChangeComplete', handleRouteChange);
+    }
+    return (): void => {
+      if (handleRouteChange) {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      }
+    };
+  }, [router.events]);
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
--- packages/docs-main/src/pages/_document.tsx
+++ packages/docs-main/src/pages/_document.tsx
@@ -1,7 +1,72 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
-import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+
+import { TagFragment, initGtm } from '@goldstack/utils-track';
+
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyHtml extends Html implements JSX.Element {
+  type: any;
+  props: any;
+  key: string | number | null;
+  setState: any;
+  forceUpdate: any;
+  state: any;
+  refs: any;
+  constructor() {
+    super();
+    this.key = null;
+  }
+}
+
+class MyHead extends Head implements JSX.Element {
+  type: any;
+  props: any;
+  key: string | number | null;
+  setState: any;
+  forceUpdate: any;
+  state: any;
+  refs: any;
+  constructor() {
+    super();
+    this.key = null;
+  }
+}
+
+class MyMain extends Main implements JSX.Element {
+  type: any;
+  props: any;
+  key: string | number | null;
+  setState: any;
+  forceUpdate: any;
+  state: any;
+  refs: any;
+  constructor() {
+    super();
+    this.key = null;
+  }
+}
+
+class MyNextScript extends NextScript implements JSX.Element {
+  type: any;
+  props: any;
+  key: string | number | null;
+  setState: any;
+  forceUpdate: any;
+  state: any;
+  refs: any;
+  constructor() {
+    super();
+    this.key = null;
+  }
+}
 
 class MyDocument extends Document {
   static async getStaticProps(ctx: any): Promise<any> {
@@ -26,6 +91,23 @@ class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render(): JSX.Element {
+    process.env.GOLDSTACK_DEPLOYMENT =
+      process.env.NEXT_PUBLIC_GOLDSTACK_DEPLOYMENT;
+
+    initGtm('UA-180192522-1');
+    return (
+      <MyHtml>
+        <MyHead>
+          {process.env.GOLDSTACK_DEPLOYMENT === 'prod' && <TagFragment />}
+        </MyHead>
+        <body>
+          <MyMain />
+          <MyNextScript />
+        </body>
+      </MyHtml>
+    );
   }
 }
 
