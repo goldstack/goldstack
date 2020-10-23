6148c585825fceea2b1e7fb02c97d6a96c956f88
Max Rohde <maxrohde.public@gmail.com>
2020-10-24 10:12:39 +1100

    Fix_issue_with_spaces_in_path

--- packages/utils-terraform/src/terraformCli.spec.ts
+++ packages/utils-terraform/src/terraformCli.spec.ts
@@ -2,12 +2,14 @@ import { mkdir } from '@goldstack/utils-sh';
 import { tf } from './terraformCli';
 import MockCloudProvider from './MockCloudProvider';
 
+import path from 'path';
+
 describe('Terraform CLI', () => {
   it('Should accept folder with spaces', async () => {
     const testDir = './goldstackLocal/cli-folder-w-space/My Dir';
     mkdir('-p', testDir);
     tf('init', {
-      dir: testDir,
+      dir: path.resolve(testDir),
       provider: new MockCloudProvider(),
     });
   });
