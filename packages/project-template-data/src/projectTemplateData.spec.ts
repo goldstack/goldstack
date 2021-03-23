3357b654f353be906080cb4498f335d20dc22ece
Max Rohde <maxrohde.public@gmail.com>
2021-03-23 18:55:27 +1100

    Add_unit_test_to_prevent_test_fail

--- packages/project-template-data/src/projectTemplateData.spec.ts
+++ packages/project-template-data/src/projectTemplateData.spec.ts
@@ -1,1 +1,10 @@
-import {} from './projectTemplateData';
+import { MoreDetails } from './projectTemplateData';
+
+describe('Project Template Data', () => {
+  it('Should be able to provide types', () => {
+    const details: MoreDetails = {
+      description: 'My description',
+    };
+    expect(details.description).toEqual('My description');
+  });
+});
