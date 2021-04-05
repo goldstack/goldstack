0544718722261ed5c094b81e70acd8fef442926e
Max Rohde <maxrohde.public@gmail.com>
2021-04-05 14:32:03 +1000

    Improving repo seed

--- packages/goldstack-api/src/utils/seedRepo.ts
+++ packages/goldstack-api/src/utils/seedRepo.ts
@@ -5,73 +5,10 @@ import { rmSafe, mkdir } from '@goldstack/utils-sh';
 import { S3TemplateRepository } from '@goldstack/template-repository';
 import { connect, getBucketName } from '@goldstack/template-repository-bucket';
 
-export interface Template {
-  getTemplateName(): string;
-}
-
-export class StaticWebsiteAwsTemplate implements Template {
-  getTemplateName(): string {
-    return 'static-website-aws';
-  }
-}
-
-export class AppNextJsTemplate implements Template {
-  getTemplateName(): string {
-    return 'app-nextjs';
-  }
-}
-
-export class AppNextJsBootstrapTemplate implements Template {
-  getTemplateName(): string {
-    return 'app-nextjs-bootstrap';
-  }
-}
-
-export class DockerImageAwsTemplate implements Template {
-  getTemplateName(): string {
-    return 'docker-image-aws';
-  }
-}
-
-export class S3Template implements Template {
-  getTemplateName(): string {
-    return 's3';
-  }
-}
-
-export class LambdaGoGinTemplate implements Template {
-  getTemplateName(): string {
-    return 'lambda-go-gin';
-  }
-}
-
-export class EmailSendTemplate implements Template {
-  getTemplateName(): string {
-    return 'email-send';
-  }
-}
-
-export class LambdaExpressTemplate implements Template {
-  getTemplateName(): string {
-    return 'lambda-express';
-  }
-}
-
-export const getDefaultTemplates = (): Template[] => {
-  return [
-    new StaticWebsiteAwsTemplate(),
-    new AppNextJsTemplate(),
-    new AppNextJsBootstrapTemplate(),
-    new DockerImageAwsTemplate(),
-    new S3Template(),
-    new LambdaExpressTemplate(),
-    new LambdaGoGinTemplate(),
-    new EmailSendTemplate(),
-  ];
-};
+import { getModuleTemplatesNames } from '@goldstack/module-template-utils';
 
 const seed = async (): Promise<void> => {
-  const templates = getDefaultTemplates();
+  const templates = getModuleTemplatesNames();
 
   const templateS3 = await connect();
   const templateRepo = new S3TemplateRepository({
@@ -92,8 +29,8 @@ const seed = async (): Promise<void> => {
   console.log('Template successfully built');
 
   for (const template of templates) {
-    console.log('Building', template.getTemplateName());
-    await build(template.getTemplateName(), {
+    console.log('Building', template);
+    await build(template, {
       monorepoRoot: './../../../../',
       destinationDirectory: './goldstackLocal/work/templates/',
       templateRepository: templateRepo,
