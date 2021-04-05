651cd2bf5bac8bc70615e945a6ace3779d3bf88b
Max Rohde <maxrohde.public@gmail.com>
2021-04-05 13:42:42 +1000

    Refactor_repo_seed

--- packages/goldstack-api/src/utils/seedRepo.ts
+++ packages/goldstack-api/src/utils/seedRepo.ts
@@ -1,10 +1,74 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('source-map-support').install();
 import { build } from '@goldstack/template-build';
-import { getDefaultTemplates } from '@goldstack/utils-template-test';
 import { rmSafe, mkdir } from '@goldstack/utils-sh';
 import { S3TemplateRepository } from '@goldstack/template-repository';
 import { connect, getBucketName } from '@goldstack/template-repository-bucket';
+
+export interface Template {
+  getTemplateName(): string;
+}
+
+export class StaticWebsiteAwsTemplate implements Template {
+  getTemplateName(): string {
+    return 'static-website-aws';
+  }
+}
+
+export class AppNextJsTemplate implements Template {
+  getTemplateName(): string {
+    return 'app-nextjs';
+  }
+}
+
+export class AppNextJsBootstrapTemplate implements Template {
+  getTemplateName(): string {
+    return 'app-nextjs-bootstrap';
+  }
+}
+
+export class DockerImageAwsTemplate implements Template {
+  getTemplateName(): string {
+    return 'docker-image-aws';
+  }
+}
+
+export class S3Template implements Template {
+  getTemplateName(): string {
+    return 's3';
+  }
+}
+
+export class LambdaGoGinTemplate implements Template {
+  getTemplateName(): string {
+    return 'lambda-go-gin';
+  }
+}
+
+export class EmailSendTemplate implements Template {
+  getTemplateName(): string {
+    return 'email-send';
+  }
+}
+
+export class LambdaExpressTemplate implements Template {
+  getTemplateName(): string {
+    return 'lambda-express';
+  }
+}
+
+export const getDefaultTemplates = (): Template[] => {
+  return [
+    new StaticWebsiteAwsTemplate(),
+    new AppNextJsTemplate(),
+    new AppNextJsBootstrapTemplate(),
+    new DockerImageAwsTemplate(),
+    new S3Template(),
+    new LambdaExpressTemplate(),
+    new LambdaGoGinTemplate(),
+    new EmailSendTemplate(),
+  ];
+};
 
 const seed = async (): Promise<void> => {
   const templates = getDefaultTemplates();
