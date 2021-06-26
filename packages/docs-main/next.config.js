89732a81896cad2071b63e53155f99c2ba36f2dc
Max Rohde <maxrohde.public@gmail.com>
2021-06-26 12:41:57 +1000

    Fixing build

--- packages/docs-main/next.config.js
+++ packages/docs-main/next.config.js
@@ -12,6 +12,12 @@ const nextConfig = {
     // ESLint managed on the workspace level
     ignoreDuringBuilds: true,
   },
+  // fixing issues with Next.js default loader and using next export
+  // https://github.com/vercel/next.js/issues/21079
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 };
 
 const config = withPlugins([[images]], nextConfig);
