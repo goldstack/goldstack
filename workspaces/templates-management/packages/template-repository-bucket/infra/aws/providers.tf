833009dddb5580dc10c4882db22af6847e84d7a5
Max Rohde <maxrohde.public@gmail.com>
2021-07-07 08:21:45 +1000

    Migrating Terraform state to S3 Remotes

--- workspaces/templates-management/packages/template-repository-bucket/infra/aws/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  version                 = "2.65.0"
-  region                  = var.aws_region
-}
--- workspaces/templates/packages/s3/infra/aws/providers.tf
+++ workspaces/templates/packages/s3/infra/aws/providers.tf
@@ -2,3 +2,9 @@ provider "aws" {
   version                 = "2.65.0"
   region                  = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    # config provided dynamically using `--backend-config` CLI parameters
+  }
+}
