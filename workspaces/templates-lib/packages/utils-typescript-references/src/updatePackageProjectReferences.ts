41289fcb47090ba92dc611733bda2ab22d441303
Max Rohde <maxrohde.public@gmail.com>
2021-10-08 08:05:31 +1100

    Fix package references creation

--- workspaces/templates-lib/packages/utils-typescript-references/src/updatePackageProjectReferences.ts
+++ workspaces/templates-lib/packages/utils-typescript-references/src/updatePackageProjectReferences.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
-import { getPackages } from './sharedUtils';
+import { getPackages, PackageData } from './sharedUtils';
 import path from 'path';
 
 export const updatePackageProjectReferences = (): void => {
@@ -12,44 +12,59 @@ export const updatePackageProjectReferences = (): void => {
 
   console.log(root);
   for (const packageData of allPackages) {
-    console.log(`Processing package ${packageData.path}`);
-    execSync(`cd ${packageData.path}`);
+    const packageDir = packageData.path;
 
-    const packageJson = fs.readFileSync('./package.json').toString();
-    const packageJsonData = JSON.parse(packageJson);
-    const tsConfig = fs.readFileSync('./tsconfig.json').toString();
-
-    const tsConfigData = JSON.parse(tsConfig);
-
-    tsConfigData.references = [
-      ...(Object.keys(packageJsonData.dependencies) || []),
-      ...(Object.keys(packageJsonData.devDependencies) || []),
-    ]
-      // all dependencies that are workspace dependencies
-      .filter(
-        (dependencyData) =>
-          allPackages.filter(
-            (packageData) => packageData.name === dependencyData
-          ).length > 0
-      )
-      // for each add a path
-      .map((dependencyData) => {
-        const dependencyPackageData = allPackages.find(
-          (packageData) => packageData.name === dependencyData
-        );
-        if (!dependencyPackageData) {
-          throw new Error('Package not found');
-        }
-        return {
-          path: path.posix.relative(
-            packageData.path,
-            dependencyPackageData.path
-          ),
-        };
-      });
-
-    console.log(tsConfigData.references);
-    // fs.writeFileSync('./tsconfig.json', JSON.stringify(tsConfigData, null, 2));
-    execSync(`cd ${root}`);
+    if (fs.existsSync(path.resolve(packageDir, './tsconfig.json'))) {
+      processPackage(packageDir, allPackages, packageData);
+    } else {
+      console.log(`Skipping package ${packageDir}`);
+    }
+    // fs.writeFileSync(
+    //   path.resolve(packageDir, './tsconfig.json'),
+    //   JSON.stringify(tsConfigData, null, 2)
+    // );
   }
 };
+
+function processPackage(
+  packageDir: string,
+  allPackages: PackageData[],
+  packageData: PackageData
+) {
+  console.log(`Processing package ${packageDir}`);
+  console.log(process.cwd());
+  const packageJson = fs
+    .readFileSync(path.resolve(packageDir, './package.json'))
+    .toString();
+  const packageJsonData = JSON.parse(packageJson);
+  const tsConfig = fs
+    .readFileSync(path.resolve(packageDir, './tsconfig.json'))
+    .toString();
+
+  const tsConfigData = JSON.parse(tsConfig);
+
+  tsConfigData.references = [
+    ...Object.keys(packageJsonData.dependencies || {}),
+    ...Object.keys(packageJsonData.devDependencies || {}),
+  ]
+    // all dependencies that are workspace dependencies
+    .filter(
+      (dependencyData) =>
+        allPackages.filter((packageData) => packageData.name === dependencyData)
+          .length > 0
+    )
+    // for each add a path
+    .map((dependencyData) => {
+      const dependencyPackageData = allPackages.find(
+        (packageData) => packageData.name === dependencyData
+      );
+      if (!dependencyPackageData) {
+        throw new Error('Package not found');
+      }
+      return {
+        path: path.posix.relative(packageData.path, dependencyPackageData.path),
+      };
+    });
+
+  console.log(tsConfigData.references);
+}
