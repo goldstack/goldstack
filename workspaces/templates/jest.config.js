581d7caf95ae81e26157b46b46970087e4059dee
Max Rohde <maxrohde.public@gmail.com>
2021-10-24 10:37:55 +1100

    Fixing unit tests

--- workspaces/templates/jest.config.js
+++ workspaces/templates/jest.config.js
@@ -2,6 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/goldstackLocal/',
+    '/distWeb/',
+    '/distLambda/',
+    '.d.ts',
+  ],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.base.json',
