449d7708782d6e6dfcc5db0228a32deebfd9e148
Max Rohde <maxrohde.public@gmail.com>
2021-10-29 07:49:49 +1100

    Adding tests for project without any deployment config

--- workspaces/apps/packages/template-management-cli/src/deploySets/deploySets.ts
+++ workspaces/apps/packages/template-management-cli/src/deploySets/deploySets.ts
@@ -2,12 +2,14 @@ import { createBackendNodejsExpressBuildSetConfig } from './backendNodejsExpress
 import { createBackendGoGinBuildSetConfig } from './backendGoGin';
 import { createStaticWebsiteBuildSetConfig } from './staticWebsite';
 import { DeploySetConfig } from '@goldstack/template-build-set';
+import { createNoInfraBuildSetConfig } from './noInfra';
 
 export const getAllBuildSets = async (): Promise<DeploySetConfig[]> => {
   return [
     await createBackendNodejsExpressBuildSetConfig(),
     await createStaticWebsiteBuildSetConfig(),
     await createBackendGoGinBuildSetConfig(),
+    await createNoInfraBuildSetConfig(),
   ];
 };
 
---
+++ workspaces/apps/packages/template-management-cli/src/deploySets/noInfra.ts
@@ -0,0 +1,114 @@
+import { ProjectConfiguration } from '@goldstack/utils-project';
+import { DeploySetConfig } from '@goldstack/template-build-set';
+
+export const createNoInfraBuildSetConfig = async (): Promise<DeploySetConfig> => {
+  const projectConfiguration: ProjectConfiguration = {
+    projectName: 'project-no-infra',
+    rootTemplateReference: {
+      templateName: 'yarn-pnp-monorepo',
+    },
+    packages: [
+      {
+        packageName: 's3-1',
+        templateReference: {
+          templateName: 's3',
+        },
+      },
+      {
+        packageName: 'lambda-express-1',
+        templateReference: {
+          templateName: 'lambda-express',
+        },
+      },
+      {
+        packageName: 'email-send-1',
+        templateReference: {
+          templateName: 'email-send',
+        },
+      },
+      {
+        packageName: 'app-nextjs-1',
+        templateReference: {
+          templateName: 'app-nextjs',
+        },
+      },
+      {
+        packageName: 'app-nextjs-bootstrap-1',
+        templateReference: {
+          templateName: 'app-nextjs-bootstrap',
+        },
+      },
+      {
+        packageName: 'static-website-1',
+        templateReference: {
+          templateName: 'static-website-aws',
+        },
+      },
+    ],
+  };
+
+  const setConfig: DeploySetConfig = {
+    buildSetName: 'backend-nodejs-express',
+    buildTemplates: [
+      'yarn-pnp-monorepo',
+      's3',
+      'lambda-express',
+      'email-send',
+      'app-nextjs',
+      'app-nextjs-bootstrap',
+      'static-website-aws',
+    ],
+    deployTemplates: [],
+    projects: [
+      {
+        projectConfiguration,
+        rootTests: ['assert-package-files', 'assert-root-files', 'root-build'],
+        packageConfigurations: [
+          {
+            packageName: 's3-1',
+            configuration: {},
+            deployments: [],
+            packageTests: ['assert-package-files'],
+            packageCleanUp: [],
+          },
+          {
+            packageName: 'lambda-express-1',
+            configuration: {},
+            deployments: [],
+            packageTests: ['assert-package-files'],
+            packageCleanUp: [],
+          },
+          {
+            packageName: 'email-send-1',
+            configuration: {},
+            deployments: [],
+            packageTests: ['assert-package-files'],
+            packageCleanUp: [],
+          },
+          {
+            packageName: 'app-nextjs-1',
+            configuration: {},
+            deployments: [],
+            packageTests: ['assert-package-files'],
+            packageCleanUp: [],
+          },
+          {
+            packageName: 'app-nextjs-bootstrap-1',
+            configuration: {},
+            deployments: [],
+            packageTests: ['assert-package-files'],
+            packageCleanUp: [],
+          },
+          {
+            packageName: 'static-website-1',
+            configuration: {},
+            deployments: [],
+            packageTests: ['assert-package-files'],
+            packageCleanUp: [],
+          },
+        ],
+      },
+    ],
+  };
+  return setConfig;
+};
