ed816a874bf80c1086d34329a4264c31b67117cc
Max Rohde <maxrohde.public@gmail.com>
2022-01-02 15:26:09 +1100

    Adding end to end test for Lambda API

--- workspaces/apps/packages/template-management-cli/src/deploySets/backendLambdaApi.ts
+++ workspaces/apps/packages/template-management-cli/src/deploySets/backendLambdaApi.ts
@@ -17,12 +17,44 @@ export const createBackendLambdaApiBuildSetConfig = async (): Promise<DeploySetC
     ],
   };
 
-  // const hash = new Date().getTime();
+  const hash = new Date().getTime();
   const setConfig: DeploySetConfig = {
     buildSetName: 'backend-lambda-api',
     buildTemplates: ['yarn-pnp-monorepo', 'lambda-api'],
     deployTemplates: ['yarn-pnp-monorepo', 'lambda-api'],
-    projects: [],
+    projects: [
+      {
+        projectConfiguration,
+        rootTests: ['assert-package-files', 'assert-root-files', 'root-build'],
+        packageConfigurations: [
+          {
+            packageName: 'lambda-api-1',
+            configuration: {},
+            deployments: [
+              {
+                name: 'prod',
+                awsUser: 'goldstack-dev',
+                awsRegion: 'us-west-2',
+                configuration: {
+                  lambdaNamePrefix: `goldstack-ci-test-lambda-api-${hash}`,
+                  apiDomain: `lambda-api-${hash}.tests.dev.goldstack.party`,
+                  hostedZoneDomain: 'dev.goldstack.party',
+                  cors: '',
+                  lambdas: {},
+                },
+              },
+            ],
+            packageTests: [
+              'assert-package-files',
+              'infra-up',
+              'deploy',
+              'assert-rest-api',
+            ],
+            packageCleanUp: ['infra-destroy'],
+          },
+        ],
+      },
+    ],
   };
   return setConfig;
 };
