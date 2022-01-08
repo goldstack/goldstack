c39321f0b00690b37d3cd489634a464203aee1a3
Max Rohde <maxrohde.public@gmail.com>
2022-01-08 16:05:38 +1100

    Making terraform faster

--- workspaces/templates/packages/lambda-api/infra/aws/providers.tf
+++ workspaces/templates/packages/lambda-api/infra/aws/providers.tf
@@ -16,7 +16,13 @@ terraform {
 }
 
 provider "aws" {
-  region                  = var.aws_region
+  region                      = var.aws_region
+
+  # Skipping various checks to speed up AWS provider
+  skip_region_validation      = true
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
 }
 
 provider "archive" {
@@ -25,5 +31,10 @@ provider "archive" {
 terraform {
   backend "s3" {
     # config provided dynamically using `--backend-config` CLI parameters
+
+    # Skipping various checks to speed up backend initialisation
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
   }
 }
