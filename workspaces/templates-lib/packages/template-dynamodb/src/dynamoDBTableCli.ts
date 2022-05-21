30f6d294c54ee743fb3020f6ccc3eae5debb7c95
Max Rohde <maxrohde.public@gmail.com>
2022-05-21 10:25:20 +1000

    Ensuring dynamodb cli operations run on different platforms

--- workspaces/templates-lib/packages/template-dynamodb/src/dynamoDBTableCli.ts
+++ workspaces/templates-lib/packages/template-dynamodb/src/dynamoDBTableCli.ts
@@ -1,13 +1,28 @@
 import { read } from '@goldstack/utils-sh';
+import { existsSync } from 'fs';
+import { posix, resolve } from 'path';
 import { connect, deleteTable, migrateDownTo } from './templateDynamoDBTable';
+
+const loadMigrations = (): any => {
+  const jsPath = resolve(
+    `${posix.dirname(process.cwd())}/dist/src/migrations.js`
+  );
+  if (existsSync(jsPath)) {
+    return require(jsPath);
+  }
+  const tsPath = resolve(`${posix.dirname(process.cwd())}/src/migrations.ts`);
+  if (existsSync(tsPath)) {
+    return require(tsPath);
+  }
+  throw new Error(`Cannot find migrations file at ${jsPath} or ${tsPath}`);
+};
 
 export const dynamoDBCli = async (args: string[]): Promise<void> => {
   const goldstackConfig = JSON.parse(read(`${process.cwd()}/goldstack.json`));
   const packageSchema = JSON.parse(
     read(`${process.cwd()}/schemas/package.schema.json`)
   );
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const migrations = require(`${process.cwd()}/src/migrations`).createMigrations();
+  const migrations = loadMigrations().createMigrations();
   if (args[0] === 'init') {
     await connect({
       goldstackConfig,
