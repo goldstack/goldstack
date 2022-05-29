11c301fbedee63a496dc03f4341474aa47d14eb0
Goldstack Project Builder <goldstack@pureleap.com>
2022-05-29 21:36:23 +0000

    GitHub actions build (ci_skip) (trigger_template_deploy_dev)

--- .pnp.cjs
+++ .pnp.cjs
@@ -2579,8 +2579,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./workspaces/templates/packages/serverless-api/",\
           "packageDependencies": [\
             ["@goldstack/serverless-api", "workspace:workspaces/templates/packages/serverless-api"],\
-            ["@goldstack/dynamodb", "workspace:workspaces/templates/packages/dynamodb"],\
-            ["@goldstack/s3", "workspace:workspaces/templates/packages/s3"],\
             ["@goldstack/template-lambda-api-cli", "workspace:workspaces/templates-lib/packages/template-lambda-api-cli"],\
             ["@goldstack/utils-aws-http-api-local", "workspace:workspaces/templates-lib/packages/utils-aws-http-api-local"],\
             ["@jest-mock/express", "npm:1.4.5"],\
