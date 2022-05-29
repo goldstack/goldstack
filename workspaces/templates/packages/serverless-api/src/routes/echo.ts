95a5f5cd3001e5f416c79865a8aafd4074ce8528
Max Rohde <maxrohde.public@gmail.com>
2022-05-29 16:27:02 +1000

    Removing test integrations

--- workspaces/templates/packages/serverless-api/src/routes/echo.ts
+++ workspaces/templates/packages/serverless-api/src/routes/echo.ts
@@ -1,20 +1,11 @@
 import { Handler, APIGatewayProxyEventV2 } from 'aws-lambda';
 
-import { connect, getBucketName } from '@goldstack/s3';
-
 type ProxyHandler = Handler<APIGatewayProxyEventV2, any>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler: ProxyHandler = async (event, context) => {
   const message = event.queryStringParameters?.message || 'no message';
 
-  const s3 = await connect();
-  const res = await s3
-    .getObject({
-      Bucket: await getBucketName(),
-      Key: 'test.txt',
-    })
-    .promise();
   if (!res.Body) {
     throw new Error('Cannot find test.txt');
   }
--- workspaces/templates/packages/serverless-api/src/routes/user/{userId}.ts
+++ workspaces/templates/packages/serverless-api/src/routes/user/{userId}.ts
@@ -4,21 +4,11 @@ import {
   APIGatewayProxyResultV2,
 } from 'aws-lambda';
 
-import { connect, getTableName } from '@goldstack/dynamodb';
-
 type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler: ProxyHandler = async (event, context) => {
   const userId = event.pathParameters?.['userId'] || 'not specified';
-
-  const client = await connect();
-
-  const res = await client
-    .scan({
-      TableName: await getTableName(),
-    })
-    .promise();
 
   return {
     statusCode: 201,
