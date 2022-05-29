75cfc59fcf68188eb4dc0cd3df8a06b72c25de35
Goldstack Project Builder <goldstack@pureleap.com>
2022-05-29 21:35:01 +0000

    Releasing new library version (ci_skip)

---
+++ workspaces/templates-lib/packages/template-lambda-api-cli/distLambda/customBuild/lambda.js
@@ -0,0 +1,3 @@
+var t=Object.defineProperty;var y=Object.getOwnPropertyDescriptor;var x=Object.getOwnPropertyNames;var P=Object.prototype.hasOwnProperty;var d=(r,e)=>{for(var a in e)t(r,a,{get:e[a],enumerable:!0})},l=(r,e,a,o)=>{if(e&&typeof e=="object"||typeof e=="function")for(let n of x(e))!P.call(r,n)&&n!==a&&t(r,n,{get:()=>e[n],enumerable:!(o=y(e,n))||o.enumerable});return r};var H=r=>l(t({},"__esModule",{value:!0}),r);var c={};d(c,{handler:()=>s});module.exports=H(c);var s=async(r,e)=>({message:"Hi"});0&&(module.exports={handler});
+// Custom config in operation
+//# sourceMappingURL=lambda.js.map
