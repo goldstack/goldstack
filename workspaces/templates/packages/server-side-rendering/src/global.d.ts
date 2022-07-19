c9be9be65980759df866fdaf1e8ba424c5bceb89
Max Rohde <maxrohde.public@gmail.com>
2022-07-20 07:50:16 +1000

    Working on supporting styles

---
+++ workspaces/templates/packages/server-side-rendering/src/global.d.ts
@@ -0,0 +1,26 @@
+declare module '*.module.css';
+
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: any;
+  export default content;
+}
--- workspaces/templates/packages/server-side-rendering/src/routes/$index.tsx
+++ workspaces/templates/packages/server-side-rendering/src/routes/$index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import styles from './$index.module.css';
+
 import {
   Handler,
   APIGatewayProxyEventV2,
