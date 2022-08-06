fbc22564a4b989f1b49012db912739fd2a965c2e
Max Rohde <maxrohde.public@gmail.com>
2022-08-07 07:25:17 +1000

    Running both SSR and UI tests

---
+++ workspaces/templates/packages/server-side-rendering/jest.config.ui.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const base = require('./../../jest.config.ui');
+
+module.exports = {
+  ...base,
+  // only run uispec tests since SSR tests do not run in jsdom environment
+  testRegex: '\\.(uispec)\\.ts[x]?$',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
+  transform: {
+    '.+\\.(css|style|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '\\.(css)$': '<rootDir>/scripts/cssTransformer.js',
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
+};
