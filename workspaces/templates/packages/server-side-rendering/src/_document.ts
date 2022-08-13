6179e8cbe6ed0a558cedfad62ea9f896eaed798a
Max Rohde <maxrohde.public@gmail.com>
2022-08-14 09:30:03 +1000

    Ensure favicon loaded correctly in all environments

--- workspaces/templates/packages/server-side-rendering/src/_document.ts
+++ workspaces/templates/packages/server-side-rendering/src/_document.ts
@@ -10,6 +10,10 @@ export const renderDocument = ({
 <html>
   <head>
     ${styles ? `<style>${styles}</style>` : ''}
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
   </head>
   <body>
     <div id="root">${renderedHtml}</div>
