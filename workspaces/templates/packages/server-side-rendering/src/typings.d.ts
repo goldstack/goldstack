31349399c6227ef6e15b4d0ae385512323ffd8d7
Max Rohde <maxrohde.public@gmail.com>
2022-09-10 06:45:33 +1000

    Adding more module types

--- workspaces/templates/packages/server-side-rendering/src/typings.d.ts
+++ workspaces/templates/packages/server-side-rendering/src/typings.d.ts
@@ -1,32 +1,29 @@
-// declare module '*.module.css';
-// declare module '*.css';
-
 declare module '*.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
 
-// declare module '*.svg' {
-//   const content: any;
-//   export default content;
-// }
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
 
-// declare module '*.png' {
-//   const content: any;
-//   export default content;
-// }
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
 
-// declare module '*.jpg' {
-//   const content: any;
-//   export default content;
-// }
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
 
-// declare module '*.jpeg' {
-//   const content: any;
-//   export default content;
-// }
+declare module '*.jpeg' {
+  const content: any;
+  export default content;
+}
 
-// declare module '*.gif' {
-//   const content: any;
-//   export default content;
-// }
+declare module '*.gif' {
+  const content: any;
+  export default content;
+}
