b8fead8cd6ab9e706ff50ff5d63746b68d6f6dc0
Max Rohde <maxrohde.public@gmail.com>
2022-12-03 09:41:24 +1100

    Adding deploy set

---
+++ workspaces/templates/packages/template-metadata/src/deploySets/userManagement.ts
@@ -0,0 +1,60 @@
+import { ProjectConfiguration } from '@goldstack/utils-project';
+import { DeploySetConfig } from '@goldstack/template-build-set';
+
+export const createUserManagementBuildSetConfig =
+  async (): Promise<DeploySetConfig> => {
+    const projectConfiguration: ProjectConfiguration = {
+      projectName: 'project-user-management',
+      rootTemplateReference: {
+        templateName: 'yarn-pnp-monorepo',
+      },
+      packages: [
+        {
+          packageName: 'user-management-1',
+          templateReference: {
+            templateName: 'user-management',
+          },
+        },
+      ],
+    };
+
+    const hash = new Date().getTime();
+    const setConfig: DeploySetConfig = {
+      buildSetName: 'user-management',
+      buildTemplates: ['yarn-pnp-monorepo', 'user-management'],
+      deployTemplates: ['user-management'],
+      projects: [
+        {
+          projectConfiguration,
+          rootTests: [
+            'assert-package-files',
+            'assert-root-files',
+            'root-build',
+          ],
+          packageConfigurations: [
+            {
+              packageName: 'user-management-1',
+              configuration: {},
+              deployments: [
+                {
+                  name: 'prod',
+                  awsUser: 'goldstack-dev',
+                  awsRegion: 'us-west-2',
+                  configuration: {
+                    cognitoDomain: `auth-integration-${hash}.dev.goldstack.party`,
+                    userPoolName: `integration-pool-${hash}`,
+                    hostedZoneDomain: 'dev.goldstack.party',
+                    callbackUrl:
+                      'https://ssr.examples.templates.dev.goldstack.party',
+                  },
+                },
+              ],
+              packageTests: ['assert-package-files', 'infra-up'],
+              packageCleanUp: ['infra-destroy'],
+            },
+          ],
+        },
+      ],
+    };
+    return setConfig;
+  };
