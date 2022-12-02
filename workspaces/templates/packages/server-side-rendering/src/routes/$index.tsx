8250c7ca2055113824b7c14a23d519f39a97e20e
Max Rohde <maxrohde.public@gmail.com>
2022-12-03 09:43:24 +1100

    Remove references to user management module

--- workspaces/templates/packages/server-side-rendering/src/routes/$index.tsx
+++ workspaces/templates/packages/server-side-rendering/src/routes/$index.tsx
@@ -5,71 +5,28 @@ import { renderPage, hydrate } from './../render';
 import Panel from './../components/Panel';
 import styles from './$index.module.css';
 
-import {
-  performClientAuth,
-  performLogout,
-  connectWithCognito,
-} from '@goldstack/user-management';
-
 const Index = (props: { message: string }): JSX.Element => {
   const [clicked, setClicked] = useState(false);
-  const [token, setToken] = useState<string | undefined | 'error'>(undefined);
-  if (!token) {
-    performClientAuth()
-      .then((token) => setToken(token?.accessToken))
-      .catch((e) => {
-        setToken('error');
-        console.log(e);
-      });
-  }
   return (
     <>
       <div
         onClick={() => {
           alert('hi');
           setClicked(true);
-          performLogout();
           // throw new Error('Havent seen this');
         }}
         className={`${styles.message}`}
       >
         {props.message}
-        {token}
       </div>
       {clicked && <Panel />}
     </>
   );
 };
 
-function getCookies(rc: string): any {
-  const list = {};
-
-  rc.split(';').forEach(function (cookie) {
-    const parts = cookie.split('=');
-    const shift = parts.shift();
-    if (!shift) {
-      return;
-    }
-    const key = shift.trim();
-    const value = decodeURI(parts.join('='));
-    if (key != '') {
-      list[key] = value;
-    }
-  });
-  return list;
-}
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler: SSRHandler = async (event, context) => {
-  let message = 'Hi there';
-  const cookies = getCookies((event.cookies || []).join(';'));
-  if (cookies.goldstack_access_token) {
-    const cognito = await connectWithCognito();
-    await cognito.validate(cookies.goldstack_access_token);
-    const idToken = await cognito.validateIdToken(cookies.goldstack_id_token);
-    message = `Hello ${idToken.email}<br>`;
-  } else {
-  }
+  const message = 'Hi there';
 
   return renderPage({
     component: Index,
