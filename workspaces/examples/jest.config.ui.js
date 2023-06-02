8311e8d905512a9087b3bbbcac23548f727386e1
Max Rohde <1448524+mxro@users.noreply.github.com>
2023-06-03 06:58:21 +1000

    Removing not necessary Jest config

--- workspaces/examples/jest.config.ui.js
+++ workspaces/examples/jest.config.ui.js
@@ -5,9 +5,4 @@ module.exports = {
   ...base,
   testEnvironment: 'jsdom',
   testRegex: '\\.(uispec|spec)\\.ts[x]?$',
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.ui.json',
-    },
-  },
 };
