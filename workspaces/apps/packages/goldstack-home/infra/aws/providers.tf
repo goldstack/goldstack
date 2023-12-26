48dee1dd1d663cc8cba585cb742ebb71baf4e685
Max Rohde <1448524+mxro@users.noreply.github.com>
2023-12-27 09:26:32 +1100

    Deploying goldstack website

--- workspaces/apps/packages/goldstack-home/infra/aws/providers.tf
+++ workspaces/apps/packages/goldstack-home/infra/aws/providers.tf
@@ -7,10 +7,10 @@ terraform {
     }
     aws = {
       source = "hashicorp/aws"
-      version = "4.48.0"
+      version = "5.31.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.6"
 }
 
 provider "aws" {
