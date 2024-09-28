27912a74c2bca7d319e05717931b50b3584321fb
Max Rohde <{ID}+{username}@users.noreply.github.com>
2024-09-28 18:17:55 +1000

    Improving Python lambda function build

--- workspaces/templates/packages/lambda-python-job/lambda/lambda.py
+++ workspaces/templates/packages/lambda-python-job/lambda/lambda.py
@@ -6,7 +6,7 @@ import marvin
 marvin.settings.openai.api_key = 'YOUR_API_KEY'
 
 def handler(event, context):
-    print(ujson.dumps([{"key": "value"}, 81, True]));
+    print(ujson.dumps([{"key": "hello world again again"}, 81, True]));
     result = "Hello World"
     return {
         'statusCode' : 200,
--- workspaces/templates/packages/lambda-python-job/scripts/package.ts
+++ workspaces/templates/packages/lambda-python-job/scripts/package.ts
@@ -1,13 +1,4 @@
-import {
-  zip,
-  exec,
-  cp,
-  rmSafe,
-  globSync,
-  read,
-  rm,
-  unzip,
-} from '@goldstack/utils-sh';
+import { exec, rmSafe, globSync, read, unzip } from '@goldstack/utils-sh';
 import { debug, info, warn } from '@goldstack/utils-log';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -20,20 +11,6 @@ function getActiveVenv(): string | null {
   return activeVenv;
 }
 
-async function getSitePackagesDir(): Promise<string> {
-  const pipShowResult = exec('python -m pip show pip');
-
-  const locationMatch = pipShowResult.match(/Location:\s+(.*)/);
-  if (locationMatch) {
-    const sitePackagesDir = locationMatch[1].trim();
-    return sitePackagesDir;
-  }
-
-  throw new Error(
-    'Unable to determine the location of site-packages directory.'
-  );
-}
-
 async function packageLambda() {
   const currentDir = process.cwd();
   if (path.basename(currentDir) !== 'lambda') {
@@ -44,8 +21,6 @@ async function packageLambda() {
   const lambdaDir = path.join(projectDir, 'lambda');
   const venvDir = path.join(lambdaDir, 'lambda_env');
   const distDir = path.join(projectDir, 'distLambda'); // Destination folder
-  const targetZip = path.join(projectDir, 'lambda.zip');
-  const lambdaFunctionFile = path.join(lambdaDir, 'lambda.py');
   const requirementsFile = path.join(lambdaDir, 'requirements.txt');
 
   // Step 1: Create distLambda directory if it doesn't exist
@@ -73,16 +48,13 @@ async function packageLambda() {
   exec(
     'docker build -t python-lambda-build --file=Dockerfile --platform=linux/amd64 .'
   );
-  exec(
-    'docker run --name=python-lambda-build --platform=linux/amd64 -d python-lambda-build'
-  );
+  exec('docker create --name python-lambda-container python-lambda-build');
 
-  // Wait for the container to start and complete its task
-  // exec('sleep 5'); // Adjust the sleep duration as needed
-
-  exec('docker cp python-lambda-build:/source.zip .');
-  exec('docker stop python-lambda-build');
-  exec('docker rm python-lambda-build');
+  if (fs.existsSync('source.zip')) {
+    await rmSafe('source.zip');
+  }
+  exec('docker cp python-lambda-container:/source.zip ./source.zip');
+  exec('docker rm python-lambda-container');
 
   await unzip({
     file: 'source.zip',
@@ -103,80 +75,6 @@ async function packageLambda() {
       await rmSafe(...toDelete);
     }
   }
-}
-
-async function packageLambdaOld() {
-  const currentDir = process.cwd();
-  if (path.basename(currentDir) !== 'lambda') {
-    throw new Error('Script expects to be run from lambda/ directory');
-  }
-
-  const projectDir = path.dirname(currentDir);
-  const lambdaDir = path.join(projectDir, 'lambda');
-  const venvDir = path.join(lambdaDir, 'lambda_env');
-  const distDir = path.join(projectDir, 'distLambda'); // Destination folder
-  const targetZip = path.join(projectDir, 'lambda.zip');
-  const lambdaFunctionFile = path.join(lambdaDir, 'lambda.py');
-  const requirementsFile = path.join(lambdaDir, 'requirements.txt');
-
-  // Step 1: Create distLambda directory if it doesn't exist
-  if (!fs.existsSync(distDir)) {
-    info('Creating distLambda directory...');
-    fs.mkdirSync(distDir);
-  }
-
-  const prevDistFiles = globSync(distDir.replace(/\\/g, '/') + '/*');
-  await rmSafe(...prevDistFiles);
-
-  // Step 2: Check if the virtual environment is already activated and mapped to lambda_env
-  const activeVenv = getActiveVenv();
-  if (!activeVenv || path.resolve(activeVenv) !== path.resolve(venvDir)) {
-    throw new Error(
-      `No virtual environment is activated or it doesn't map to ${venvDir}. Please activate the correct virtual environment.`
-    );
-  }
-
-  info(`Virtual environment ${activeVenv} is active and maps to lambda_env.`);
-
-  // Step 3: Export a fresh requirements.txt from the currently active virtual environment
-  info('Exporting a fresh requirements.txt...');
-  exec(`python -m pip freeze > ${requirementsFile}`);
-
-  // Step 4: Determine site-packages directory in a platform-independent way
-  const sitePackagesDir = await getSitePackagesDir();
-
-  if (!fs.existsSync(sitePackagesDir)) {
-    throw new Error(`Site-packages directory not found in: ${sitePackagesDir}`);
-  }
-
-  // Step 5: Copy dependencies and lambda function to distLambda directory
-  info('Copying dependencies and lambda function to distLambda...');
-  cp('-rf', path.join(sitePackagesDir), distDir);
-  cp('-f', lambdaFunctionFile, path.join(distDir, 'lambda.py'));
-
-  // Clean out excluded packages
-  const deployConfig = JSON.parse(read(path.join(lambdaDir, 'deploy.json')));
-  for (const excludedPackage of deployConfig.excludePackages) {
-    const pattern =
-      distDir.replace(/\\/g, '/') + '/site-packages/' + excludedPackage;
-    debug(`Searching for excluded package using pattern: ${pattern}`);
-    const toDelete = globSync(pattern);
-    if (toDelete.length > 0) {
-      debug(
-        `Deleting excluded package ${excludedPackage} in ${toDelete.join(', ')}`
-      );
-      await rmSafe(...toDelete);
-    }
-  }
-
-  // Step 6: Zip the distLambda directory
-
-  // Not necessary, done by library
-
-  // info('Zipping distLambda contents...');
-  // await zip({ directory: distDir, target: targetZip });
-
-  info(`Lambda package created successfully at: ${targetZip}`);
 }
 
 packageLambda()
