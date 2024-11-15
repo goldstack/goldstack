9469b77ead5632877dd47e8c9347253e6624a437
Max Rohde <{ID}+{username}@users.noreply.github.com>
2024-11-15 11:52:23 +1100

    Improving way SQS mocks are being managed

--- workspaces/templates-lib/packages/template-sqs/src/sqsConnect.spec.ts
+++ workspaces/templates-lib/packages/template-sqs/src/sqsConnect.spec.ts
@@ -1,16 +1,25 @@
-import { SendMessageCommand } from '@aws-sdk/client-sqs';
+import { SendMessageCommand, SendMessageRequest } from '@aws-sdk/client-sqs';
 import {
   getSentMessageRequests,
   connect,
   getMockedDLQSQS,
+  getSQSQueueUrl,
+  getSQSDLQQueueUrl,
+  getMockedSQS,
 } from './templateSqs';
 
 describe('SQS connect', () => {
   it('Should connect to mocked SQS', async () => {
-    const sqs = await connect({}, {}, 'local');
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const sqs = getMockedSQS({ name: 'template-sqs' }, () => {});
     await sqs.send(
       new SendMessageCommand({
-        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue',
+        QueueUrl: await getSQSQueueUrl(
+          { name: 'template-sqs' },
+          {},
+          {},
+          'local'
+        ),
         MessageBody: 'This is a test message',
       })
     );
@@ -23,15 +32,62 @@ describe('SQS connect', () => {
     const sqs = getMockedDLQSQS({ name: 'template-sqs' });
     await sqs.send(
       new SendMessageCommand({
-        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/123456789012/MyDLQQueue',
+        QueueUrl: await getSQSDLQQueueUrl(
+          { name: 'template-sqs' },
+          {},
+          {},
+          'local'
+        ),
         MessageBody: 'This is a second test message',
       })
     );
 
     const sentMessageRequests = getSentMessageRequests(sqs);
-    expect(sentMessageRequests).toHaveLength(2);
-    expect(sentMessageRequests[1].MessageBody).toBe(
+    expect(sentMessageRequests).toHaveLength(1);
+    expect(sentMessageRequests[0].MessageBody).toBe(
       'This is a second test message'
+    );
+  });
+  it('Should connect to multiple mocked SQS', async () => {
+    const callbackHandledRequests1: SendMessageRequest[] = [];
+
+    const callbackHandledRequests2: SendMessageRequest[] = [];
+    const sqs1 = getMockedSQS({ name: 'template-sqs-1' }, (request) => {
+      callbackHandledRequests1.push(request);
+    });
+    const sqs2 = getMockedSQS({ name: 'template-sqs-2' }, (request) => {
+      callbackHandledRequests2.push(request);
+    });
+    await sqs1.send(
+      new SendMessageCommand({
+        QueueUrl: await getSQSQueueUrl(
+          { name: 'template-sqs-1' },
+          {},
+          {},
+          'local'
+        ),
+        MessageBody: 'This is a test message for queue 1',
+      })
+    );
+
+    sqs2.send(
+      new SendMessageCommand({
+        QueueUrl: await getSQSQueueUrl(
+          { name: 'template-sqs-2' },
+          {},
+          {},
+          'local'
+        ),
+        MessageBody: 'This is a test message for queue 2',
+      })
+    );
+
+    expect(callbackHandledRequests1).toHaveLength(1);
+    expect(callbackHandledRequests2).toHaveLength(1);
+    const sentMessageRequests = getSentMessageRequests(sqs1);
+    expect(sentMessageRequests).toHaveLength(1);
+    expect(sentMessageRequests[0].MessageBody).toBe(
+      'This is a test message for queue 1'
     );
   });
 });
