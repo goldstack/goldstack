86a01cf5700a56d2c141330e5f33205043c3ba29
Max Rohde <{ID}+{username}@users.noreply.github.com>
2024-12-17 14:46:30 +1100

    Removing not required styling

--- workspaces/templates/packages/server-side-rendering/tailwind.config.js
+++ workspaces/templates/packages/server-side-rendering/tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      fontFamily: {
-        robotoslab: ['Roboto Slab', 'serif'],
-        hindvadodara: ['Hind Vadodara', 'sans-serif'],
-        serif: ['Roboto Slab', 'serif'],
-        sans: ['Hind Vadodara', 'sans-serif'],
-      },
+      fontFamily: {},
       // via https://www.tints.dev
       colors: {
         black: {
@@ -24,34 +19,6 @@ module.exports = {
           800: '#000A0A',
           900: '#000505',
           950: '#000000',
-        },
-        white: {
-          50: '#FEFCFB',
-          100: '#FEF8F6',
-          200: '#FCEEE9',
-          300: '#FAE7E0',
-          400: '#F8DDD3',
-          500: '#F7D7CA',
-          DEFAULT: '#F7D7CA',
-          600: '#EB9A7A',
-          700: '#E0612F',
-          800: '#9B3D17',
-          900: '#501F0C',
-          950: '#281006',
-        },
-        orange: {
-          50: '#FFF0EB',
-          100: '#FFE5DB',
-          200: '#FFC8B3',
-          300: '#FFAF8F',
-          400: '#FF9166',
-          500: '#FF7642',
-          DEFAULT: '#FF7642',
-          600: '#FF4800',
-          700: '#C23700',
-          800: '#802400',
-          900: '#421300',
-          950: '#1F0900',
         },
       },
     },
