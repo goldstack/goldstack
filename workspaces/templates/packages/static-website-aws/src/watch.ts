6a35971d3b737d77e77a5bacdcebda660cb07e46
Max Rohde <{ID}+{username}@users.noreply.github.com>
2024-12-23 13:38:44 +1100

    Fixing compile errors

--- workspaces/templates/packages/static-website-aws/src/watch.ts
+++ workspaces/templates/packages/static-website-aws/src/watch.ts
@@ -5,7 +5,7 @@ import { init } from './gulp';
 const watch = async (args: string[]): Promise<void> => {
   const gulp = await init(args);
   await new Promise<void>((resolve) => {
-    gulp.series(['watch'])(resolve);
+    gulp.series(['watch'])(resolve as any);
   });
 };
 
