088974fffc69590aa4822aa84deb88204df6c2e0
Max Rohde <1448524+mxro@users.noreply.github.com>
2025-01-05 16:49:04 +1100

    Fixing issue with lambda zip path when deploying lambda

--- workspaces/templates-lib/packages/utils-aws-lambda/src/deployFunction.ts
+++ workspaces/templates-lib/packages/utils-aws-lambda/src/deployFunction.ts
@@ -1,8 +1,9 @@
-import { awsCli } from '@goldstack/utils-aws-cli';
+import { awsCli, hasAwsCli } from '@goldstack/utils-aws-cli';
 import { zip, rmSafe } from '@goldstack/utils-sh';
 import { getAWSCredentials } from '@goldstack/infra-aws';
 import { AwsCredentialIdentityProvider } from '@aws-sdk/types';
 import { debug } from '@goldstack/utils-log';
+import { relative, sep, isAbsolute } from 'path';
 
 export interface DeployFunctionParams {
   lambdaPackageDir: string;
@@ -30,12 +31,29 @@ export const deployFunction = async (
 
   let fixedTargetArchive = targetArchive;
 
-  const isWin = process.platform === 'win32';
+  if (!hasAwsCli()) {
+    // When running in Docker via AWS CLI, we need to use paths relative to the mounted /app directory
+    debug(`Original target archive path: ${targetArchive}`);
 
-  if (!isWin) {
+    // If it's an absolute path, make it relative to the working directory
+    if (isAbsolute(fixedTargetArchive)) {
+      fixedTargetArchive = relative(process.cwd(), fixedTargetArchive);
+    }
+
+    // Normalize to forward slashes and escape special characters
+    fixedTargetArchive = fixedTargetArchive.split(sep).join('/');
     fixedTargetArchive = fixedTargetArchive.replace(/\$/g, '\\$');
+
+    debug(`Fixed target archive path: ${fixedTargetArchive}`);
   } else {
-    fixedTargetArchive = fixedTargetArchive.replaceAll('\\', '\\\\');
+    // When running without Docker, we need to ensure correct paths for Windows
+    const isWin = process.platform === 'win32';
+
+    if (!isWin) {
+      fixedTargetArchive = fixedTargetArchive.replace(/\$/g, '\\$');
+    } else {
+      fixedTargetArchive = fixedTargetArchive.replaceAll('\\', '\\\\');
+    }
   }
 
   const deployResult = await awsCli({
