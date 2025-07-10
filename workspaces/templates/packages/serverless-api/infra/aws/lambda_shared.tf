f5ae8fa181dae293cbebdbd4c58fe20a650a34ea
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-07-11 08:21:26 +1000

    Fix doubled up tf resource

--- workspaces/templates/packages/serverless-api/infra/aws/lambda_shared.tf
+++ workspaces/templates/packages/serverless-api/infra/aws/lambda_shared.tf
@@ -27,34 +27,3 @@ resource "aws_iam_role_policy_attachment" "lambda_admin_role_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# Explicit roles to allow logging for Lambda. Not strictly required here due to the full admin access
-# granted in the lambda_admin_role_attach above. But added here to make it easier to fine-tune permissions
-# in the above at a later point. 
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda-api-logging-role-${random_id.id.hex}"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
-}
-
