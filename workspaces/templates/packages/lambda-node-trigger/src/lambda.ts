8d8120d660ccd09e7a2d68ced46562dbcb347737
Goldstack Project Builder <goldstack@pureleap.com>
2025-08-11 04:21:18 +0000

    Updates from build pipeline (ci_skip)

--- workspaces/templates/packages/lambda-node-trigger/src/lambda.ts
+++ workspaces/templates/packages/lambda-node-trigger/src/lambda.ts
@@ -1,7 +1,7 @@
-require("source-map-support").install();
+require('source-map-support').install();
 
-import type { SendMessageRequest, SQSClient } from "@aws-sdk/client-sqs";
-import type { MessageCallback } from "@goldstack/template-sqs";
+import type { SendMessageRequest, SQSClient } from '@aws-sdk/client-sqs';
+import type { MessageCallback } from '@goldstack/template-sqs';
 import {
   getSQSDLQQueueName as fetchDLQQueueName,
   getSQSDLQQueueUrl as fetchDLQQueueUrl,
@@ -10,17 +10,17 @@ import {
   connect as templateConnect,
   getMockedDLQSQS as templateGetMockedDLQSQS,
   getMockedSQS as templateGetMockedSQS,
-} from "@goldstack/template-sqs";
-import { warn } from "@goldstack/utils-log";
-import type { Context, ScheduledEvent, SQSEvent } from "aws-lambda";
-import goldstackConfig from "./../goldstack.json";
-import goldstackSchema from "./../schemas/package.schema.json";
-import { handler as lambdaHandler } from "./handler";
-import deployments from "./state/deployments.json";
+} from '@goldstack/template-sqs';
+import { warn } from '@goldstack/utils-log';
+import type { Context, ScheduledEvent, SQSEvent } from 'aws-lambda';
+import goldstackConfig from './../goldstack.json';
+import goldstackSchema from './../schemas/package.schema.json';
+import { handler as lambdaHandler } from './handler';
+import deployments from './state/deployments.json';
 
 export const handler = async (
   event: ScheduledEvent | SQSEvent,
-  context: Context
+  context: Context,
 ): Promise<void> => {
   return await lambdaHandler(event, context);
 };
@@ -34,23 +34,21 @@ export const handler = async (
  * @returns {SQSClient} The mocked SQS client.
  */
 export const getMockedSQS = (): SQSClient => {
-  const messageSendHandler: MessageCallback = async (
-    message: SendMessageRequest
-  ) => {
+  const messageSendHandler: MessageCallback = async (message: SendMessageRequest) => {
     // Constructing a mock event to pass to the handler
     const sqsEvent: SQSEvent = {
       Records: [
         {
           body: message.MessageBody as string,
           // Other required fields can be filled as needed
-          messageId: "mockMessageId",
-          receiptHandle: "mockReceiptHandle",
+          messageId: 'mockMessageId',
+          receiptHandle: 'mockReceiptHandle',
           attributes: {} as any,
           messageAttributes: {},
-          md5OfBody: "mockMd5",
-          eventSource: "aws:sqs",
-          eventSourceARN: "mockARN",
-          awsRegion: "mockRegion",
+          md5OfBody: 'mockMd5',
+          eventSource: 'aws:sqs',
+          eventSourceARN: 'mockARN',
+          awsRegion: 'mockRegion',
         },
       ],
     };
@@ -70,47 +68,31 @@ export const getMockedSQS = (): SQSClient => {
  * @returns {SQSClient} The mocked SQS client.
  */
 export const getMockedDLQSQS = (): SQSClient => {
-  const messageSendHandler: MessageCallback = async (
-    message: SendMessageRequest
-  ) => {
-    warn("DLQ Message received " + message.MessageBody);
+  const messageSendHandler: MessageCallback = async (message: SendMessageRequest) => {
+    warn('DLQ Message received ' + message.MessageBody);
   };
 
   return templateGetMockedDLQSQS(goldstackConfig, messageSendHandler);
 };
 
-export const connectToSQSDLQQueue = async (
-  deploymentName?: string
-): Promise<SQSClient> => {
-  deploymentName = deploymentName || process.env["GOLDSTACK_DEPLOYMENT"];
+export const connectToSQSDLQQueue = async (deploymentName?: string): Promise<SQSClient> => {
+  deploymentName = deploymentName || process.env['GOLDSTACK_DEPLOYMENT'];
 
-  if (deploymentName === "local") {
+  if (deploymentName === 'local') {
     return getMockedDLQSQS();
   }
 
-  return await templateConnect(
-    goldstackConfig,
-    goldstackSchema,
-    deployments,
-    deploymentName
-  );
+  return await templateConnect(goldstackConfig, goldstackSchema, deployments, deploymentName);
 };
 
-export const connectToSQSQueue = async (
-  deploymentName?: string
-): Promise<SQSClient> => {
-  deploymentName = deploymentName || process.env["GOLDSTACK_DEPLOYMENT"];
+export const connectToSQSQueue = async (deploymentName?: string): Promise<SQSClient> => {
+  deploymentName = deploymentName || process.env['GOLDSTACK_DEPLOYMENT'];
 
-  if (deploymentName === "local") {
+  if (deploymentName === 'local') {
     return getMockedSQS();
   }
 
-  return await templateConnect(
-    goldstackConfig,
-    goldstackSchema,
-    deployments,
-    deploymentName
-  );
+  return await templateConnect(goldstackConfig, goldstackSchema, deployments, deploymentName);
 };
 
 /**
@@ -118,15 +100,8 @@ export const connectToSQSQueue = async (
  *
  * @returns {Promise<string>} The name of the SQS queue.
  */
-export async function getSQSQueueName(
-  deploymentName?: string
-): Promise<string> {
-  return await fetchQueueName(
-    goldstackConfig,
-    goldstackSchema,
-    deployments,
-    deploymentName
-  );
+export async function getSQSQueueName(deploymentName?: string): Promise<string> {
+  return await fetchQueueName(goldstackConfig, goldstackSchema, deployments, deploymentName);
 }
 
 /**
@@ -135,12 +110,7 @@ export async function getSQSQueueName(
  * @returns {Promise<string>} The URL of the SQS queue.
  */
 export async function getSQSQueueUrl(deploymentName?: string): Promise<string> {
-  return await fetchQueueUrl(
-    goldstackConfig,
-    goldstackSchema,
-    deployments,
-    deploymentName
-  );
+  return await fetchQueueUrl(goldstackConfig, goldstackSchema, deployments, deploymentName);
 }
 
 /**
@@ -148,15 +118,8 @@ export async function getSQSQueueUrl(deploymentName?: string): Promise<string> {
  *
  * @returns {Promise<string>} The name of the SQS DLQ queue.
  */
-export async function getSQSDLQQueueName(
-  deploymentName?: string
-): Promise<string> {
-  return await fetchDLQQueueName(
-    goldstackConfig,
-    goldstackSchema,
-    deployments,
-    deploymentName
-  );
+export async function getSQSDLQQueueName(deploymentName?: string): Promise<string> {
+  return await fetchDLQQueueName(goldstackConfig, goldstackSchema, deployments, deploymentName);
 }
 
 /**
@@ -164,13 +127,6 @@ export async function getSQSDLQQueueName(
  *
  * @returns {Promise<string>} The URL of the SQS DLQ queue.
  */
-export async function getSQSDLQQueueUrl(
-  deploymentName?: string
-): Promise<string> {
-  return await fetchDLQQueueUrl(
-    goldstackConfig,
-    goldstackSchema,
-    deployments,
-    deploymentName
-  );
+export async function getSQSDLQQueueUrl(deploymentName?: string): Promise<string> {
+  return await fetchDLQQueueUrl(goldstackConfig, goldstackSchema, deployments, deploymentName);
 }
