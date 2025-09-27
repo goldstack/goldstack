ca34901a45a4beac6f1f33498a31dced68f21bbf
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-09-27 09:43:11 +0700

    Looking at workspaces/templates/app-nextjs and workspaces/templates/app-nextjs-bootstrap
    propose the basic package files for workspaces/templates/app-nextjs-tailwind

---
+++ workspaces/templates/packages/app-nextjs-tailwind/README.template.md
@@ -0,0 +1,5 @@
+# Next.js Template
+
+❤️ Check out the [Goldstack Project Builder](https://goldstack.party) to use this template ❤️
+
+[!embed](../../../docs/docs/templates/app-nextjs/index.md)
---
+++ workspaces/templates/packages/app-nextjs-tailwind/src/__tests__/index.uispec.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Homepage from '../../pages/index';
+
+test('Check App component render', () => {
+  const res = render(<Homepage />);
+
+  expect(res.getByText('Toggle Data Display', { exact: false })).toBeVisible();
+});
