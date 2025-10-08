96f33b88134d676d106fe100238c793c693ed86b
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-10-08 10:50:32 +0700

    Setting lang attribute for html

--- workspaces/templates/packages/app-nextjs-tailwind/src/pages/_document.tsx
+++ workspaces/templates/packages/app-nextjs-tailwind/src/pages/_document.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
