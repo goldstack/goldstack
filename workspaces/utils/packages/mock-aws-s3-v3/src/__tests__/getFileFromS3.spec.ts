baaa53b7b6fdd6ec58858e065454fc47272ab122
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-11-28 12:11:58 +0700

    Adding test cases

--- workspaces/utils/packages/mock-aws-s3-v3/src/__tests__/getFileFromS3.spec.ts
+++ workspaces/utils/packages/mock-aws-s3-v3/src/__tests__/getFileFromS3.spec.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { GetObjectCommand, NoSuchKey, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { createS3Client } from 'mock-aws-s3-v3';
 
 test('Can retrieve file using getFileFromS3 function if it exists', async () => {
@@ -18,8 +18,58 @@ test('Can retrieve file using getFileFromS3 function if it exists', async () => 
 
   const response = await getFileFromS3('bucket', 'some-file', client);
 
-  console.log(response);
   expect(await response.Body?.transformToString()).toBe('test content');
+
+  async function getFileFromS3(bucket: string, key: string, s3Client: S3Client) {
+    const response = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: bucket,
+        Key: key,
+      }),
+    );
+    return response;
+  }
+});
+
+test('Can retrieve file with empty content using getFileFromS3 function if it exists', async () => {
+  const client = createS3Client({
+    localDirectory: 's3Mock',
+    bucket: 'bucket',
+  });
+
+  // Put an object first so getFileFromS3 can retrieve it
+  await client.send(
+    new PutObjectCommand({
+      Bucket: 'bucket',
+      Key: 'some-other-file',
+      Body: '',
+    }),
+  );
+
+  const response = await getFileFromS3('bucket', 'some-other-file', client);
+
+  expect(await response.Body?.transformToString()).toBe('');
+
+  async function getFileFromS3(bucket: string, key: string, s3Client: S3Client) {
+    const response = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: bucket,
+        Key: key,
+      }),
+    );
+    return response;
+  }
+});
+
+test('Correct error when accessing not existing object', async () => {
+  const client = createS3Client({
+    localDirectory: 's3Mock',
+    bucket: 'bucket',
+  });
+
+  await expect(getFileFromS3('bucket', 'some-key-that-does-not-exist', client)).rejects.toThrow(
+    NoSuchKey,
+  );
 
   async function getFileFromS3(bucket: string, key: string, s3Client: S3Client) {
     const response = await s3Client.send(
