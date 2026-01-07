b777668ce470a0983bb75717174a4e809edf221b
Max Rohde <{ID}+{username}@users.noreply.github.com>
2026-01-07 18:47:59 +1100

    Updating react testing

--- workspaces/templates/packages/app-nextjs/src/__tests__/index.uispec.tsx
+++ workspaces/templates/packages/app-nextjs/src/__tests__/index.uispec.tsx
@@ -1,6 +1,10 @@
-import Homepage from '../../pages/index';
+import { render } from '@testing-library/react';
+import Homepage from "../pages";
+import '@testing-library/jest-dom';
 
-test('Check App component render', () => {
-  expect(Homepage).toBeDefined();
-  expect(typeof Homepage).toBe('function');
+test('Check App component render', async () => {
+  const res = render(<Homepage />);
+
+  expect((await res.findAllByText(/toggle data display/i)).length).toBeGreaterThan(0)
+
 });
