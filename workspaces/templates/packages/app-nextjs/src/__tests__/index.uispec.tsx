7c93a8c4750b5b04b68266e91e4c4e0402291217
Max Rohde <{ID}+{username}@users.noreply.github.com>
2026-01-07 18:51:07 +1100

    Fixing issues

--- workspaces/templates/packages/app-nextjs/src/__tests__/index.uispec.tsx
+++ workspaces/templates/packages/app-nextjs/src/__tests__/index.uispec.tsx
@@ -1,10 +1,9 @@
 import { render } from '@testing-library/react';
-import Homepage from "../pages";
+import Homepage from '../pages';
 import '@testing-library/jest-dom';
 
 test('Check App component render', async () => {
   const res = render(<Homepage />);
 
-  expect((await res.findAllByText(/toggle data display/i)).length).toBeGreaterThan(0)
-
+  expect((await res.findAllByText(/toggle data display/i)).length).toBeGreaterThan(0);
 });
