3aa0bc0ee49fe26bb6e2ad6a40ae32236a48748b
Max Rohde <1448524+mxro@users.noreply.github.com>
2026-03-12 14:43:00 +1100

    Adding limits for API invocations

--- workspaces/templates/packages/serverless-api/infra/aws/api_gateway.tf
+++ workspaces/templates/packages/serverless-api/infra/aws/api_gateway.tf
@@ -23,4 +23,10 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+
+  # Conservative default limits for requests, adjust for your usecase as required
+  default_route_settings {
+    throttling_rate_limit  = 30 
+    throttling_burst_limit = 60
+  }
 }
