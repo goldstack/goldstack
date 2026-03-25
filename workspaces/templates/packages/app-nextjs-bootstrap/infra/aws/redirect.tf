5d2261da6869a37f7f1a5bca8e409a8a04b30b82
Max Rohde <1448524+mxro@users.noreply.github.com>
2026-03-25 15:41:11 +1100

    fix: correct CloudFront origin request policy ID

--- workspaces/templates/packages/app-nextjs-bootstrap/infra/aws/redirect.tf
+++ workspaces/templates/packages/app-nextjs-bootstrap/infra/aws/redirect.tf
@@ -126,7 +126,7 @@ resource "aws_cloudfront_distribution" "website_cdn_redirect" {
     viewer_protocol_policy   = "redirect-to-https" # Redirects any HTTP request to HTTPS
     compress                 = true
     cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
-    origin_request_policy_id = "88a5eaf4-2fd4-4709-b84d-a0c0ba98654c" # CORS-S3Origin
+    origin_request_policy_id = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf" # CORS-S3Origin
 
     function_association {
       event_type   = "viewer-request"
