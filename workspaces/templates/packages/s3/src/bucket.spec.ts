33edaa9434d460aa9a38fc3024f3e03e11dae6cc
Max Rohde <1448524+mxro@users.noreply.github.com>
2026-04-05 12:16:42 +1000

    removing test that needs prod bucket

--- workspaces/templates/packages/s3/src/bucket.spec.ts
+++ workspaces/templates/packages/s3/src/bucket.spec.ts
@@ -9,21 +9,6 @@ describe('S3 Bucket', () => {
     expect(bucketName).toEqual('goldstack-test-s3-bucket-new');
   });
 
-  it('Should be able to upload file', async () => {
-    if (!process.env.AWS_ACCESS_KEY_ID) {
-      console.warn('Testing of S3 upload skipped since no AWS credentials available');
-      return;
-    }
-    const s3 = await connect('prod');
-    const bucketName = await getBucketName('prod');
-    const cmd = new PutObjectCommand({
-      Key: 'test.txt',
-      Body: 'hello',
-      Bucket: bucketName,
-    });
-    await s3.send(cmd);
-  });
-
   it('Should get local bucket name', async () => {
     const bucketName = await getBucketName();
     expect(bucketName).toEqual('local-s3');
