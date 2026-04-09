34ef91350741f7d2d84ffa54fd8f5750b69e1f62
Max Rohde <1448524+mxro@users.noreply.github.com>
2026-04-09 11:33:27 +1000

    enabling versioning, so backups work without error

--- workspaces/templates/packages/app-nextjs/infra/aws/root.tf
+++ workspaces/templates/packages/app-nextjs/infra/aws/root.tf
@@ -19,6 +19,26 @@ resource "aws_s3_bucket" "website_root" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "website_root" {
+  bucket = aws_s3_bucket.website_root.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "website_root" {
+  bucket = aws_s3_bucket.website_root.id
+
+  rule {
+    id     = "delete-noncurrent-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "website_root" {
   bucket = aws_s3_bucket.website_root.id
 
