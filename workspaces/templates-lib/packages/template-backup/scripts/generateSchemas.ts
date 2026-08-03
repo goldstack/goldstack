b8fcd4c159dd03de11c70e70836e79cb35c11846
Max Rohde <1448524+mxro@users.noreply.github.com>
2026-08-03 14:09:44 +0200

    Fix generateSchemas.ts: use import { argv } from 'process' instead of global process

--- workspaces/templates-lib/packages/template-backup/scripts/generateSchemas.ts
+++ workspaces/templates-lib/packages/template-backup/scripts/generateSchemas.ts
@@ -1,4 +1,4 @@
 import { run } from '@goldstack/utils-package-config-generate';
+import { argv } from 'process';
 
-// @ts-ignore TS6 + ts-node doesn't resolve process global for scripts outside project scope
-run(process.argv);
+run(argv);
